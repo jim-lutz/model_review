f774a1fe9afc8e00eea82aa35750660fad9d291c
jim-lutz <jdlutz@hotwaterresearch.net>
2021-09-28 16:40:26 -0700

    Qcond.cyl() and Qcond.disc() put int ExtTempFunc.R

--- ExtTempFunc.R
+++ ExtTempFunc.R
@@ -1,13 +1,14 @@
 # ExtTempFunc.R
 # functions called by ExtTempCalc.Rmd
 
-# Properties of Air
+# Functions of Properties of Air
 # linear fit to cover ~ 20 °C to 75 °C
 # from https://www.engineeringtoolbox.com/
 # see ../Peer Review/Properties_of_Air.ods
 
 beta <- function(
-  # coefficient of thermal expansion of air, 1/K  
+  # coefficient of thermal expansion of air, 1/K 
+  # used in Grashof number
   Tair = 20 # air temperature, °C 
   # default beta = 3.43e-3  
 ){
@@ -17,6 +18,7 @@ beta <- function(
 
 nu <- function(
   # kinematic viscosity of air, (m2/s), using air at 22.5 °C
+  # used in Grashof number and Prandtl number
   Tair = 22.5 # air temperature, °C 
   # default nu = 15.28e-6 
 ){
@@ -26,6 +28,7 @@ nu <- function(
 
 alpha <- function(
   # Air thermal diffusivity at atmospheric pressure, (m2/s), using air at 22.5 °C
+  # used in Prandtl number
   Tair = 22.5 # air temperature, °C 
   # default alpha =  22.045e-6 
 ){
@@ -35,13 +38,49 @@ alpha <- function(
 
 
 k_air <- function(
-    # Air thermal conductivity at atmospheric pressure, (W/m-K)
-    Tair = 22.5 # air temperature, °C 
-    # default k_air = 26.06e-3
+  # Air thermal conductivity at atmospheric pressure, (W/m-K)
+  # used with Nusselt number to calculate convective heat transfer coefficients
+  Tair = 22.5 # air temperature, °C 
+  # default k_air = 26.06e-3
   ){
   k_air = 0.071967176004528 * Tair + 24.4579117147708
   return(k_air)
   }
 
 
+# Functions for conductive heat transfer
+# from 2021 ASHRAE® HANDBOOK FUNDAMENTALS, SI Edition
+# CHAPTER 4 HEAT TRANSFER
+# Table 2 One-Dimensional Conduction Shape Factors
 
+Qcond.cyl <- function( 
+  # function to calculate the heat transfer through 
+  # hollow cylinder of length L with negligible heat transfer from end surfaces
+  length,      # length of cylinder, m
+  r_outer,     # outer radius of cylinder, m
+  r_inner,     # inner radius of cylinder, m
+  T_inner = 75, # temperature of inner surface, °C
+  T_outer,      # temperature of outer surface, °C
+  k = 0.025     # thermal conductivity of cylinder,  W/m-K, default to polyurethane
+)
+{
+  Qcond.cyl = ( 2 * pi * k * length *(T_inner - T_outer)) / 
+    ( log(r_outer/r_inner) )
+  
+  return(Qcond.cyl)
+}
+
+Qcond.disc <- function( 
+  # function to calculate conductive heat transfer through a disc
+  radius,       # radius of disc, m
+  thick,        # thickness of cylinder, m
+  Thot = 75,    # hot surface temperature, °C
+  Tcold,        # cold surface temperature, °C
+  k = k_ins     # thermal conductivity of disc, W/m-K
+)
+{
+  Qcond.disc = ( (k_ins * pi * radius^2) / (thick)) * (Thot - Tcold)
+  
+  return(Qcond.disc)
+}
+  
